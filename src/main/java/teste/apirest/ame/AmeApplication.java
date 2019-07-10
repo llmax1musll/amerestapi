15fad4d0d2c9f18913811b9150eda4c17bbb6b2e
Rodrigo Macedo <rodrigolima.18@hotmail.com>
2019-07-10 15:24:37 -0300

    Criando modelo e upando para heroku

--- src/main/java/teste/apirest/ame/AmeApplication.java
+++ src/main/java/teste/apirest/ame/AmeApplication.java
@@ -2,13 +2,20 @@ package teste.apirest.ame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class AmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmeApplication.class, args);
-		System.out.println("oi");
+		//System.out.println("oi");
+	}
+	@RequestMapping
+	@ResponseBody
+	String home() {
+		return "index";
 	}
 
 }
