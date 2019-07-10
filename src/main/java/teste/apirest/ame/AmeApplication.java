18f783ae3fca2d0611bc65854c299bdf7b4685cb
Rodrigo Macedo <rodrigolima.18@hotmail.com>
2019-07-10 16:17:53 -0300

    adicionando o findall no controllerplanetas

--- src/main/java/teste/apirest/ame/AmeApplication.java
+++ src/main/java/teste/apirest/ame/AmeApplication.java
@@ -10,12 +10,7 @@ public class AmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmeApplication.class, args);
-		//System.out.println("oi");
-	}
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "index";
-	}
+		
+	}	
 
 }
